c8ba4db8fcf9916d4e912f83c94f48c13bb48373
DrakeJin <dydwls121200@gmail.com>
2020-12-31 01:25:38 +0900

    개 똥같은 레거시 삭 to tha 제

--- modules/ec2/files/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/root/init_ssh.sh ${github_accounts} >> /root/debug-init_ssh.sh.log 2>&1
--- modules/ec2/template.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "ec2" {
-  template = "${file("${path.module}/files/setup.sh")}"
-
-  vars = {
-    github_accounts = var.github_accounts
-  }
-}
